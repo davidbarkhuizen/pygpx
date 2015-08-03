751223dc9cb0492581afeae3fb1ccf993f53f115
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-08-03 06:40:55 +0200

    gpx.py - added method to give locations @ time

--- gpx.py
+++ gpx.py
@@ -1,7 +1,49 @@
+from operator import itemgetter, attrgetter, methodcaller
+
 class GPX(object):
 
-    def __init__(self, metadata, tracks, waypoints):
+	def __init__(self, metadata, tracks, waypoints):
 
-        self.metadata = metadata
-        self.tracks = tracks
-        self.waypoints = waypoints
+		self.metadata = metadata
+		self.tracks = tracks
+		self.waypoints = waypoints
+
+	def get_locations_at_time(self, time):
+
+		locations = []
+
+		track_points = []
+
+		# aggregate
+		#
+		for track in self.tracks:
+			for track_segment in track.segments:
+				track_points.extend(track_segment.points)
+
+		# sort by time
+		track_points = sorted(track_points, key=attrgetter('time'))
+
+		before = []
+		at = []
+		after = []
+
+		for p in track_points:
+
+			if p.time < time:
+				before.append(p)
+			elif p.time == time:
+				at.append(p)
+			else:
+				after.append(p)
+
+		if len(at) > 0:
+			return at
+		else:
+			locations = []
+			if len(before) > 0:
+				immediately_before = before[-1]
+				locations.append(immediately_before)
+			if len(after) > 0:
+				immediately_after = after[0]
+				locations.append(immediately_after)
+			return locations
